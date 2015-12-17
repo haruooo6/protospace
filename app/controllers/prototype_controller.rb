a01940a78b662b94f4aa029614cea94c1eff5176
haruo <haruka@watanabeharunatsu-no-MacBook-Air.local>
2015-12-17 18:34:04 +0900

    reviewいただいた箇所の修正 不要なファイルを削除、ハッシュ記法に修正、topコントローラーとprototypeコントローラーを削除

--- app/controllers/prototype_controller.rb
+++
@@ -1,3 +0,0 @@
-class PrototypeController < ApplicationController
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :prototypes
   resources :users, only: [:index]
-  root "top#index"
+  root "prototypes#index"
 end
