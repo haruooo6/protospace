e8ff170af320bb75530e63a54072f56d2112234b
haruo <haruka@watanabeharunatsu-no-MacBook-Air.local>
2015-12-21 01:43:00 +0900

    ユーザーのedit画面を入れ替え、修正した。usersテーブルにgroupカラムの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:nickname, :profile, :works) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:nickname, :profile, :works, :avatar)}
   end
 end
 
