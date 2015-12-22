fe1805c8654e42945642872af06a3bb42e872200
haruo <haruka@watanabeharunatsu-no-MacBook-Air.local>
2015-12-22 13:43:36 +0900

    reviewしていただいた部分を修正 パスをprefixに修正、空白

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:nickname, :profile, :works) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:nickname, :profile, :works, :avatar)}
+    devise_parameter_sanitizer.for(:sign_up).push(:nickname, :profile, :works)
+    devise_parameter_sanitizer.for(:account_update).push(:nickname, :profile, :works, :avatar)
   end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root "prototypes#index"
   devise_for :users
   resources :prototypes
-  resources :users, only: [:show, :edit]
-  root "prototypes#index"
+  resources :users, only: :show
 end
