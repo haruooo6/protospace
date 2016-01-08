d7c30aecdd40457b4fe800e11f217e7fb310c012
haruo <haruka@watanabeharunatsu-no-MacBook-Air.local>
2016-01-08 18:56:38 +0900

    mount_uploaderの修正 と トップページで投稿したものが表示されるようにした。

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,9 @@
 class Picture < ActiveRecord::Base
   belongs_to :prototype
   accepts_nested_attributes_for :prototype
-  mount_uploader :picture, PicutureUploader
+  mount_uploader :main, PicutureUploader
+  mount_uploader :sub1, PicutureUploader
+  mount_uploader :sub2, PicutureUploader
+  mount_uploader :sub3, PicutureUploader
   enum picture: [:main, :sub1, :sub2, :sub3]
 end
