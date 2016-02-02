028fcb22dc5608184f9f4ddfe5d9e3dcefe2b4d1
haruo <haruka@watanabeharunatsu-no-MacBook-Air.local>
2016-02-02 23:44:41 +0900

    Ajaxを使いlikeの数が増えるようにした

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,7 @@ class LikesController < ApplicationController
   def create
     @like = Like.create(prototype_id: params[:prototype_id])
     @likes = Like.where(prototype_id: params[:prototype_id])
-    redirect_to :back
+    @prototype = Prototype.find(params[:prototype_id])
   end
 
   def destroy
