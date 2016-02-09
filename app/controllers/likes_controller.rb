e356a32323827e7fe611f1e87835089175e9f3be
haruo <haruka@watanabeharunatsu-no-MacBook-Air.local>
2016-02-09 14:09:14 +0900

    reviewをいただいた箇所の修正 ・_like.html.hamoのファイルの位置を変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,14 @@
 class LikesController < ApplicationController
   def create
     Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
-    @likes = Like.where(id_params)
+    @likes = Like.where(user_id: current_user.id, prototype_id: params[:prototype_id])
     @prototype = Prototype.find(params[:prototype_id])
   end
 
   def destroy
-    @like = current_user.likes.find_by(id_params)
-    @like.destroy
-    @prototype = Prototype.find(params[:prototype_id])
+    like = Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id]).destroy
     @likes = Like.where(id_params)
+    @prototype = Prototype.find(params[:prototype_id])
   end
 
   private
