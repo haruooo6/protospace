9e096beb55f582378c9fafaa837929f1a13d798e
haruo <haruka@watanabeharunatsu-no-MacBook-Air.local>
2016-02-09 14:07:11 +0900

    reviewをいただいた箇所の修正 ・コメントcreateをアソシエーションを使って記述 ・_comment.html.hamlのファイルの位置を変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,11 @@
 class CommentsController < ApplicationController
   def create
     @prototype = Prototype.find(params[:prototype_id])
-    Comment.create(comment_params)
+    current_user.comments.create(comment_params)
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:comment).merge(prototype_id: params[:prototype_id], user_id: current_user.id)
+    params.require(:comment).permit(:comment).merge(prototype_id: params[:prototype_id])
   end
 end
