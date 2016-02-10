98f0bf2b54a0e01a9ec068dc42f5760cc8a7daca
haruo <haruka@watanabeharunatsu-no-MacBook-Air.local>
2016-02-10 13:45:49 +0900

    reviewして頂いた箇所を修正 ・アソシエーションを使って簡潔に記述

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,14 @@
 class LikesController < ApplicationController
   def create
     Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
-    @likes = Like.where(user_id: current_user.id, prototype_id: params[:prototype_id])
     @prototype = Prototype.find(params[:prototype_id])
+    @likes = @prototype.likes
   end
 
   def destroy
-    like = Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id]).destroy
-    @likes = Like.where(id_params)
+    Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id]).destroy
     @prototype = Prototype.find(params[:prototype_id])
+    @likes = @prototype.likes
   end
 
   private
